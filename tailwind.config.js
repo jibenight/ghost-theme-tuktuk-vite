15ed25a26e271c87867d349f7ced475de397b770
Jean <nguyen.jean@me.com>
2025-05-11 07:16:48 +0200

    use link dist

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  content: ['./**/*.hbs', './assets/js/**/*.js'],
+  theme: {
+    extend: {
+      // Ajoute ici tes couleurs, polices, breakpoints personnalisés si nécessaire
+    },
+  },
+  plugins: [],
+};
